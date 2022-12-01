12e51af339f1845511e6fdaf0a8e99e311c5f86b
F2BEAR <facucarbonel@hotmail.com>
2022-12-01 14:46:47 -0300

    header fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 
 export default function App() {
-  return (
-    <div className='w-full min-h-screen text-purple-100 p-12 flex flex-col justify-between gap-8'>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
-  )
+	return (
+		<div className="w-full min-h-screen text-purple-100 p-12 flex justify-center content-center">
+			<div className='flex flex-col justify-between gap-8'>
+				<Header />
+				<Home />
+				<Footer />
+			</div>
+		</div>
+	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import easyQR from '../assets/easyqr.svg'
 export default function Header() {
   return (
-    <header className="flex flex-row justify-center lg:justify-start items-center text-center gap-2 lg:ml-64 lg:pl-4">
+    <header className="w-full flex flex-row justify-center lg:justify-start items-center text-center gap-2 lg:mx-3">
       <img src={easyQR} alt="Logo" className="w-7 h-7" />
       <h1 className="text-4xl font-bold">EasyQR</h1>
     </header>
